b6716c0952acb8839310b44742612396f3c30aff
mikegoen <mikejg4@gmail.com>
2016-01-16 13:31:18 -0800

    change font size

--- calc.js
+++ calc.js
@@ -1,52 +1,84 @@
-var inputTotal = [];
+var inputTotal = [],
+    newEntry = true;
 
 $('.button').click(function () {
   var input = $(this).html();
   var display = $('#display-text').html();
-  var newInput = true;
   input = input.trim();
 
-  if (input == 'C') { //clear
-    $('#display-text').html('0');
-    inputTotal = [];
-  } else if (input == 'CE') {
-    $('#display-text').html(display.slice(0, display.length-1));
-    inputTotal.pop();
-    if ($('#display-text').html() == '') $('#display-text').html('0')
-  } else if (+input >= 0 && display == '0') { //first input
+//first input
+  if (+input >= 0 && newEntry == true) {
     $('#display-text').html('');
     $('#display-text').html(input);
     inputTotal.push(+input);
-  } else if (+input >= 0) {           //second or greater input
+    newEntry = false;
+  }
+//second or greater input
+  else if (+input >= 0) {
     $('#display-text').html(display + String(input));
     inputTotal.push(+input)
-  } else if (input == '=') {
-    $('#display-text').html(eval( inputTotal.join('') ) );
-    inputTotal = [];
-  } else {
-    parseOperator(input, display);
+  }
+
+  else {
+    nonNumber(input, display);
   }
 });
 
-function parseOperator(input, display) {
+function nonNumber(input, display) {
+
+  //carry over from previous calculation
+  if (newEntry) {
+    inputTotal.push(display);
+    newEntry = false;
+  }
+
   switch (input) {
+    case 'C':
+      $('#display-text').html('0');
+      inputTotal = [];
+      newEntry = true;
+      break;
+
+    case 'CE':
+      $('#display-text').html(display.slice(0, display.length-1));
+      inputTotal.pop();
+
+      //return to zero if empty
+      if ($('#display-text').html() == '') {
+        $('#display-text').html('0');
+        newEntry = true;
+        }
+      break;
+
+    case '=':
+      $('#display-text').html(eval( inputTotal.join('') ) );
+      inputTotal = [];
+      newEntry = true;
+      break;
+
     case '+':
       inputTotal.push(input);
       $('#display-text').html(display + input);
       break;
+
     case '-':
       inputTotal.push(input);
       $('#display-text').html(display + input);
       break;
+
     case 'x':
       inputTotal.push('*');
       $('#display-text').html(display + 'x');
       break;
+
     case '/':
       inputTotal.push(input);
       $('#display-text').html(display + input);
       break;
-    case '&#8730':
-      $('#display-text').html(Math.sqrt(display));
+
+    case '.':
+      inputTotal.push(input);
+      $('#display-text').html(display + input);
+      break;
     }
 }
